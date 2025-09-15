c2602889277b713a52c5063350bf3315db31bf84
Nicolas <nicolaskmbrietzig@gmail.com>
2025-09-15 23:25:38 +0000

    Publisher azure

---
+++ eventos/src/main/java/br/univille/Publisher.java
@@ -0,0 +1,33 @@
+package br.univille;
+
+import com.azure.core.amqp.AmqpTransportType;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
+
+public class Publisher {
+    public static void main(String[] args) {
+        var topicName = "topic-das1-b";
+        var servidor = "sbdas12025a.servicebus.windows.net";
+
+        // DefaultAzureCredential credential = 
+        //     new DefaultAzureCredentialBuilder().build();
+
+        String chave = System.getenv("CHAVE");
+
+        ServiceBusSenderClient senderClient =  
+            new ServiceBusClientBuilder()
+                .fullyQualifiedNamespace(servidor)
+                .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
+                // .credential(credential)
+                .connectionString(chave)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+
+        senderClient.sendMessage(new ServiceBusMessage("nicolas: mandando UMA UNIDADE de json"));
+ 
+    }
+}
